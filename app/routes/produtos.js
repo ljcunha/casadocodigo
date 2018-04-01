c255da8bd8b735dbc317caf9b95d918ec8986d9b
ASGARD\ljcun <ljcunha@gmail.com>
2018-04-01 19:29:05 -0300

    Conexão com SQL Server realizada.

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -3,7 +3,28 @@
 module.exports = function(app){
     console.log("carregando rota de produtos");
     app.get('/produtos', function(req,res){
-        console.log("Listando...");
-        res.render("produtos/lista");
+    console.log("Listando...");
+
+    var mssql = require('mssql');  
+
+    var pool = new mssql.ConnectionPool({
+        user: 'developer',
+        password: 'K@t@n@07',
+        server: 'ASGARD',  
+        database: 'CasaDoCodigo',
+        options: {
+            instanceName: 'SQLEXPRESS'
+        }
+    }); 
+
+    pool.connect()
+        .then(function(){
+            new mssql.Request(pool).query('Select * from livro',(err,result) => {
+                res.send(result);
+            });
+        })
+        .catch(function(err){
+            console.log(err);
+        });   
     });
 }
